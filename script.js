2b57c5f24f562afbe53dd046048c73a047b02929
Alexander Selivanov <mr.alex.arus@gmail.com>
2024-08-30 09:59:37 +0000

    Update style.css url in script every 5 min

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const appendStyle = () => {
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const five_minute_timestamp = date.setMinutes(minutes - minutes % 5, 0, 0);
+    const rnd = five_minute_timestamp.toString().slice(0, 8);
+
     const linkEl = document.createElement("link");
     linkEl.setAttribute('rel', 'stylesheet');
-    linkEl.setAttribute('href', 'https://alexarus.github.io/doc_tools/style.css');
+    linkEl.setAttribute('href', `https://alexarus.github.io/doc_tools/style.css?rnd=${rnd}`);
     document.head.appendChild(linkEl);
 }
 
